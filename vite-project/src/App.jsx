5579819d58c940b5b23d261a1bc0c3cb9d8b906f
Aya8Merai <aya5312.merai@gmail.com>
2025-07-27 10:29:09 +0300

    feat(vite-project): add get data using fetch as a comment

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -41,6 +41,27 @@ function App() {
         <p>{'This may take a moment.'}</p>
       </> }
     </div>)
+    
+  //   const getData = async() => {
+  //     try {
+  //       const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }        
+  //       const json = await response.json()
+  //       setPostsData(json);
+  //       // console.log(response.data);
+  //     }
+  //     catch (error) {
+  //       console.error("Error fetching posts:", error);
+  //       setErrorMessage(error.message)
+  //     }
+  //   }
+  
+  // useEffect(() => {
+  //   getData()    
+  // }, [reloadState]);
+
   
   useEffect(() => {
     axios
